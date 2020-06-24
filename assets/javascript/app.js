5217eb70ff5c34de3800fcbf09197c1535efb630
andrewwestgard <westgaar9629@uwec.edu>
2020-06-24 13:42:51 -0500

    added a map feature in HTML and CSS and created default app display. Pseudo coded JS event listners

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,57 @@
-var mapsAPI = "AIzaSyBNw2dup2_xHRjLKzsXNzhX5RtrDbx99fY"
+const mapsAPI = "AIzaSyBNw2dup2_xHRjLKzsXNzhX5RtrDbx99fY"
 
-geocodingURL: "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key="mapsAPI""
+
+geocodingURL: "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=" + mapsAPI;
+function searchBreweries(){
+    $.ajax({
+        url: "https://api.openbrewerydb.org/breweries?by_city=Minneapolis&by_type=micro&per_page=50",
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+    })
+};
+
+searchBreweries();
+
+
+
+
+
+
+
+
+//EVENT LISTNERS
+// add event listener to change to map view 
+
+    // if info-card button is pressed
+        // display of info-card = block
+        // display of mapView = none    
+    // else
+        //change display of info-card to none
+        //change display of mapView to block
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
