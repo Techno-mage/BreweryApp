2355db1c04e4caf52dd4de346acba7f79224bd1f
Erik Schaal <schaa077@umn.edu>
2020-06-24 13:32:23 -0500

    search results are now displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 const mapsAPI = "AIzaSyBNw2dup2_xHRjLKzsXNzhX5RtrDbx99fY"
 
+var results = [];
 
 geocodingURL: "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=" + mapsAPI;
 function searchBreweries(target){
@@ -8,7 +9,8 @@ function searchBreweries(target){
         method: "GET"
     }).then(function (response) {
         console.log(response);
-
+        results = response;
+        displayResults();
     })
 };
 
@@ -20,4 +22,34 @@ $("#search-button").on("click", function(){
     var target = $("#search-bar").val();
     console.log(target);
     searchBreweries(target);
+    //displayResults();
 })
+
+function displayResults(){
+    console.log(results);
+    for (result of results){
+        var name = result.name;
+        var street = result.street;
+        console.log(result.city)
+        var city = result.city;
+        console.log(city)
+        var state = result.state;
+        var phone = result.phone;
+        var site = result.website_url;
+        var info = $("<div>").addClass("p-wrapper");
+        var phoneEl = $("<p>").addClass("card-text").html("Phone: <span>"+phone+"</span>");
+        var siteEl = $("<p>").addClass("card-text").html("Website: <a>"+site+"</a>");
+        info.append(phoneEl, siteEl);
+        var body = $("<div>").addClass("card-body");
+        body.append($("<h6>").addClass("card-title").text(city + ", " + state));
+        body.append($("<p>").addClass("card-text").text(street + ", "+city+", " + state));
+        body.append(info);
+        var card = $("<div>").addClass("card");
+        card.append($("<h5>").addClass("card-header").text(name), body);
+
+        $("#info-card").append(card);
+
+        
+
+    }
+}
