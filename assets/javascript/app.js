a723a8da194222e1df497e471ca597078f034b65
Kyle Rudderforth <kruddzz@gmail.com>
2020-06-24 13:55:01 -0500

    app.js update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,9 +2,9 @@ const mapsAPI = "AIzaSyBNw2dup2_xHRjLKzsXNzhX5RtrDbx99fY"
 
 
 geocodingURL: "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=" + mapsAPI;
-function searchBreweries(){
+function searchBreweries(target){
     $.ajax({
-        url: "https://api.openbrewerydb.org/breweries?by_city=Minneapolis&by_type=micro&per_page=50",
+        url: "https://api.openbrewerydb.org/breweries?by_city="+target+"&by_type=micro&per_page=50",
         method: "GET"
     }).then(function (response) {
         console.log(response);
@@ -12,4 +12,12 @@ function searchBreweries(){
     })
 };
 
-searchBreweries();
+//searchBreweries();
+
+$("#search-button").on("click", function(){
+    event.preventDefault();
+    console.log("button clicked");
+    var target = $("#search-bar").val();
+    console.log(target);
+    searchBreweries(target);
+})
