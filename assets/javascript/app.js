b3e8252b6da65d6cc0f8e42eb3dbbad4cff6e591
andrewwestgard <westgaar9629@uwec.edu>
2020-06-24 11:37:28 -0500

    added id's to the various card elements for JS manipulation and deleted conflicted html page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
 var mapsAPI = "AIzaSyBNw2dup2_xHRjLKzsXNzhX5RtrDbx99fY"
 
-geocodingURL: "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key="mapsAPI""
+//geocodingURL: "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key="mapsAPI""
+
