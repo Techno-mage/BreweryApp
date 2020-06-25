87f6e1c7001854bbcaa245167caf10de8b10a152
Kyle Rudderforth <kruddzz@gmail.com>
2020-06-25 08:46:23 -0500

    inital map with geolocation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,13 @@
-const mapsAPI = "AIzaSyBNw2dup2_xHRjLKzsXNzhX5RtrDbx99fY"
+var results = [];
 
-
-geocodingURL: "https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=" + mapsAPI;
 function searchBreweries(target){
     $.ajax({
         url: "https://api.openbrewerydb.org/breweries?by_city="+target+"&by_type=micro&per_page=50",
         method: "GET"
     }).then(function (response) {
         console.log(response);
-
+        results = response;
+        displayResults();
     })
 };
 
@@ -20,4 +19,81 @@ $("#search-button").on("click", function(){
     var target = $("#search-bar").val();
     console.log(target);
     searchBreweries(target);
+    //displayResults();
 })
+
+function displayResults(){
+    console.log(results);
+    for (result of results){
+        var name = result.name;
+        var street = result.street;
+        console.log(result.city)
+        var city = result.city;
+        console.log(city)
+        var state = result.state;
+        var phone = result.phone;
+        var site = result.website_url;
+        var info = $("<div>").addClass("p-wrapper");
+        var phoneEl = $("<p>").addClass("card-text").html("Phone: <span>"+phone+"</span>");
+        var siteEl = $("<p>").addClass("card-text").html("Website: <a>"+site+"</a>");
+        info.append(phoneEl, siteEl);
+        var body = $("<div>").addClass("card-body");
+        body.append($("<h6>").addClass("card-title").text(city + ", " + state));
+        body.append($("<p>").addClass("card-text").text(street + ", "+city+", " + state));
+        body.append(info);
+        var card = $("<div>").addClass("card");
+        card.append($("<h5>").addClass("card-header").text(name), body);
+
+        $("#info-card").append(card);
+
+        
+
+    }
+}
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+var map, infoWindow;
+function initMap() {
+    // map options
+    var mapProp= {
+      center: new google.maps.LatLng(44.9778, -93.2650),
+      zoom:10,
+    };
+    // new map
+    map = new google.maps.Map(document.getElementById("mapDisplay"), mapProp);
+    infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+    
+
+
+
